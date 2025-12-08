bdf96490c5cedf0535bcbf6b6ec534c1167f726a
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-12-08 14:09:23 +0100

    ändrade namn

--- frontend/src/components/ToDoItem.tsx
+++ frontend/src/components/ToDoItem.tsx
@@ -1,9 +1,9 @@
 
-type ToDoProps = {
+interface Props {
 	id: number
 	text: string
 }
 
-export default function ToDoItem({ id, text }: ToDoProps) {
+export default function ToDoItem({ id, text }: Props) {
 	return <div></div>
 }
