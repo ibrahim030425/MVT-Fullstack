06e51e3848fa26849e5cf036c89d0593aafb62d9
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-12-18 14:48:38 +0100

    lagt till fler komponent tester

--- frontend/cypress/component/ToDoItem.cy.tsx
+++ frontend/cypress/component/ToDoItem.cy.tsx
@@ -14,4 +14,46 @@ describe("ToDoItem", () => {
 
     cy.contains("handla");
   });
+
+  it("visar överstruken text när completed är true", () => {
+  cy.mount(
+    <ToDoItem
+      id={1}
+      text="träna"
+      completed={true}
+      toggleComplete={() => {}}
+      removeToDo={() => {}}
+    />
+  );
+
+  cy.contains("träna")
+    .parent()
+    .should("have.css", "text-decoration-line", "line-through");
+});
+
+it("kan rendera flera todos", () => {
+  cy.mount(
+    <ul>
+      <ToDoItem
+        id={1}
+        text="torka"
+        completed={false}
+        toggleComplete={() => {}}
+        removeToDo={() => {}}
+      />
+      <ToDoItem
+        id={2}
+        text="städa"
+        completed={true}
+        toggleComplete={() => {}}
+        removeToDo={() => {}}
+      />
+    </ul>
+  );
+
+  cy.contains("torka");
+  cy.contains("städa")
+});
+
+
 });
