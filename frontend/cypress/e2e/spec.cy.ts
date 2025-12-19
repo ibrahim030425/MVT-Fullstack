4c1c1258b031ac46493884bd765e2fd0f9ba8066
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-12-19 13:59:44 +0100

    lagt till och strukturerat e2e tester

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,23 +1,13 @@
 describe('Todo Sida', () => {
   beforeEach(() => { cy.visit('http://localhost:5173/') })
 
-    it("uppdateras när todos togglas", () => {
-    cy.get('[data-cy="completed-counter"]')
-      .should("contain.text", "0 av");
-
-    cy.get("input").type("Köpa mjölk");
-    cy.contains("Lägg Till").click();
-
-    cy.get('[data-cy="completed-counter"]')
-      .should("contain.text", "0 av 1");
-
-    cy.contains("Köpa mjölk").click();
-
-    cy.get('[data-cy="completed-counter"]')
-      .should("contain.text", "1 av 1");
-  });
-
-  it('Kan inte lägga till samma todo två gånger', () => {
+ describe("Validering", () => {
+     it("kan inte lägga till tom todo", () => {
+      cy.get("input").type("   ");
+      cy.contains("Lägg Till").click();
+      cy.get("li").should("not.exist");
+});
+    it('Kan inte lägga till samma todo två gånger', () => {
     cy.get("input").type("Köpa kött")
     cy.contains("Lägg Till").click()
 
@@ -28,7 +18,9 @@ describe('Todo Sida', () => {
     })
   })
 
-  it('Lägger till och tar bort todo', () => {
+  });
+  describe("Todo-hantering", () => {
+    it('Lägger till och tar bort todo', () => {
     cy.get("input").type("Köpa mjölk")
     cy.contains("Lägg Till").click()
 
@@ -36,4 +28,39 @@ describe('Todo Sida', () => {
 
     cy.contains("Köpa mjölk").should("not.exist")
   })
-})
+   it("uppdateras när todos togglas", () => {
+    cy.get('[data-cy="completed-counter"]')
+      .should("contain.text", "0 av");
+
+    cy.get("input").type("Köpa mjölk");
+    cy.contains("Lägg Till").click();
+
+    cy.get('[data-cy="completed-counter"]')
+      .should("contain.text", "0 av 2");
+
+    cy.contains("Köpa mjölk").click();
+
+    cy.get('[data-cy="completed-counter"]')
+      .should("contain.text", "1 av 2");
+  });
+  });
+
+
+  describe("Beteenden", () => {
+    it("rensar input efter att todo lagts till", () => {
+  cy.get("input").type("Diska");
+  cy.contains("Lägg Till").click();
+
+  cy.get("input").should("have.value", "");
+});
+   it("behåller todos efter sidladdning", () => {
+  cy.get("input").type("Träna");
+  cy.contains("Lägg Till").click();
+
+  cy.reload();
+
+  cy.contains("Träna").should("exist");
+});
+
+  });
+});
